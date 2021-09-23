44e3352a7ab0c7083411648a08a31a1fcf860815
Zenif-NIght <41389677+Zenif-NIght@users.noreply.github.com>
2021-09-23 02:41:25 -0600

    got thee plots

--- FALL2021/HW1/hw2_b.py
+++ FALL2021/HW1/hw2_b.py
@@ -2,13 +2,8 @@
 # to cover a space of 5m x 5m. Plot the resulting path (x, y) 
 # and trajectory (x, y, and angular velocities).
 
-
 import matplotlib.pyplot as plt
-# import matplotlib.animation as animation
 import numpy as np
-
-# import pygame
-
 
 bot_width = .30 # m
 bot_length = .50 # m
@@ -19,7 +14,7 @@ x_side_length = 5
 y_side_length = 5
 area_to_cover =x_side_length * y_side_length # 5x5 =25 m^2
 
-# time_duration , vl, vr
+# plan [time_duration , vl, vr]
 go_straight_up = np.array([y_side_length,1,1])
 go_straight_down = np.array([y_side_length,-1,-1])
 turn_r_180 = np.array([1,turn_arc/1,0])
@@ -29,29 +24,46 @@ turn_l_180 = np.array([1,-turn_arc/1,0])
 execution_plan =[] 
 for i in range(0,int((x_side_length/bot_width)/2+1)):
 
-    print('Iteration:',i)
+    # print('Iteration:',i)
     execution_plan.append(go_straight_up)
     execution_plan.append(turn_r_180)
     execution_plan.append(go_straight_down)
     execution_plan.append(turn_l_180)
 
-t_step = 0.001 
+t_step = 0.01 
 
 colors = ['r', 'g', 'b', 'm', 'y']
 LINEWIDTH=3 #0.8
-fig, ax= plt.subplots(1,1)#plt.figure()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+fig2 = plt.figure()
+ax_2 = fig2.add_subplot(311)
+ax_3 = fig2.add_subplot(312)
+ax_4 = fig2.add_subplot(313)
+
+xDot = 0 
+yDot = 0
+Psi_dot = 0
 
 x = 0 
 y = 0
 
+xDot_list =[]
+yDot_list =[]
+Psi_dot_list =[]
+
 execution_plan = np.array(execution_plan)
-print(len(execution_plan))
+# print(len(execution_plan))
 plotLIst = []
 for i in range(0,len(execution_plan)):
     
     psi = 0 
+
     xlist =[]
     ylist =[]
+    
     
     time_duration = execution_plan[i][0]
     max_time = time_duration
@@ -60,13 +72,20 @@ for i in range(0,len(execution_plan)):
         xlist = np.append(xlist,x)
         ylist = np.append(ylist,y)
         
+        xDot_list = np.append(xDot_list,xDot)
+        yDot_list = np.append(yDot_list,yDot)
+        Psi_dot_list = np.append(Psi_dot_list,Psi_dot)
         vl = execution_plan[i][1]
         vr = execution_plan[i][2]
         
         Psi_dot = (vr - vl)/ bot_width
         psi =  psi + Psi_dot *t_step
-        x +=  -((vl + vr)/2 )* np.sin(psi)  * t_step#-v *np.sin(psi) * t_step
-        y += ((vl + vr)/2 )* np.cos(psi)  * t_step#  v* np.cos(psi) * t_step
+        xDot = -((vl + vr)/2 )* np.sin(psi) 
+        yDot = ((vl + vr)/2 )* np.cos(psi) 
+        
+        x += xDot * t_step#-v *np.sin(psi) * t_step
+        y += yDot * t_step#  v* np.cos(psi) * t_step
+        
 
     ax.plot(xlist, ylist,marker='o' , linewidth=LINEWIDTH)
     
@@ -82,10 +101,20 @@ ax.plot(0,0,marker='D')
 ax.plot(0,5,marker='D')
 ax.plot(5,5,marker='D')
 ax.plot(5,0,marker='D')
-# ax.plt.xlim(-1, 1)
-# ax.plt.ylim(-1, 1)
-ax.axis('scaled') #plt.gca().set_aspect('equal', adjustable='box')
+ax.axis('scaled') 
+
+vel_linewidth = 1
+
+ax_2.set_ylabel('X velocity')
+ax_3.set_ylabel('Y velocity')
+ax_4.set_ylabel('Ψ velocity')
+
+ax_4.set_xlabel('Time 0.01 Seconds')
+
+ax_2.plot( xDot_list , linewidth=vel_linewidth)
+ax_3.plot( yDot_list  , linewidth=vel_linewidth)
+ax_4.plot( Psi_dot_list  , linewidth=vel_linewidth)
 
 
-# plt.ioff()
+plt.ioff()
 plt.show()
