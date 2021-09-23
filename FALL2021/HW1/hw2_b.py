e6bbcf6d9005cb9499f81ae6c7077cbc328fa5bd
Zenif-NIght <41389677+Zenif-NIght@users.noreply.github.com>
2021-09-23 01:38:28 -0600

    finshed 2

--- FALL2021/HW1/hw2_b.py
+++ FALL2021/HW1/hw2_b.py
@@ -4,60 +4,72 @@
 
 
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+# import matplotlib.animation as animation
 import numpy as np
 
-import pygame
-
-
-# time_duration = [10,2,2,2,2] # sec
-# vl = [5,0,2,0,2] # m/s
-# vr = [5,2,0,2,2] # m/s
-
+# import pygame
 
 
 bot_width = .30 # m
 bot_length = .50 # m
 
-turn_arc = 2 * np.pi * (bot_width/2)/2 #half arc Circumference
-area_to_cover =25 # 5x5 =25 m^2
+turn_arc = np.pi * (bot_width) #half arc Circumference
 
-time_duration = [.01,1,1] # sec
-vl = [1,0,turn_arc/time_duration[1]] # m/s
-vr = [1,turn_arc/time_duration[0],0] # m/s
+x_side_length = 5
+y_side_length = 5
+area_to_cover =x_side_length * y_side_length # 5x5 =25 m^2
 
+# time_duration , vl, vr
+go_straight_up = np.array([y_side_length,1,1])
+go_straight_down = np.array([y_side_length,-1,-1])
+turn_r_180 = np.array([1,turn_arc/1,0])
+turn_l_180 = np.array([1,-turn_arc/1,0])
+
+
+execution_plan =[] 
+for i in range(0,int((x_side_length/bot_width)/2+1)):
+
+    print('Iteration:',i)
+    execution_plan.append(go_straight_up)
+    execution_plan.append(turn_r_180)
+    execution_plan.append(go_straight_down)
+    execution_plan.append(turn_l_180)
 
 t_step = 0.001 
 
-
 colors = ['r', 'g', 'b', 'm', 'y']
-LINEWIDTH=0.8
+LINEWIDTH=3 #0.8
 fig, ax= plt.subplots(1,1)#plt.figure()
-# ax = fig.add_subplot(121)
-# ax_2 = fig.add_subplot(122)
 
 x = 0 
 y = 0
-for i in range(0,len(time_duration)):
-    print('Test number: ', i)
+
+execution_plan = np.array(execution_plan)
+print(len(execution_plan))
+plotLIst = []
+for i in range(0,len(execution_plan)):
+    
     psi = 0 
     xlist =[]
     ylist =[]
     
-    max_time = time_duration[i]
+    time_duration = execution_plan[i][0]
+    max_time = time_duration
     for dt in np.arange(0.0, max_time,t_step):
         
         xlist = np.append(xlist,x)
         ylist = np.append(ylist,y)
         
-        Psi_dot = (vr[i] - vl[i])/ bot_width
+        vl = execution_plan[i][1]
+        vr = execution_plan[i][2]
+        
+        Psi_dot = (vr - vl)/ bot_width
         psi =  psi + Psi_dot *t_step
-        x +=  -((vl[i] + vr[i])/2 )* np.sin(psi)  * t_step#-v *np.sin(psi) * t_step
-        y += ((vl[i] + vr[i])/2 )* np.cos(psi)  * t_step#  v* np.cos(psi) * t_step
+        x +=  -((vl + vr)/2 )* np.sin(psi)  * t_step#-v *np.sin(psi) * t_step
+        y += ((vl + vr)/2 )* np.cos(psi)  * t_step#  v* np.cos(psi) * t_step
 
-    legend_str = ''f'time duration: {time_duration[i]}, Left V≈ {round(vl[i],3)}, Right V≈ {round(vr[i],3)}'
-    ax.plot(xlist, ylist, colors[i],marker='o' ,label=legend_str, linewidth=LINEWIDTH)
-    ax.legend(loc = "upper left")
+    ax.plot(xlist, ylist,marker='o' , linewidth=LINEWIDTH)
+    
     ax.spines['left'].set_position('zero')
     ax.spines['right'].set_color('none')
     ax.spines['bottom'].set_position('zero')
@@ -66,13 +78,14 @@ for i in range(0,len(time_duration)):
 ax.plot(.3/2,0,marker='o')
 ax.plot(-.3/2,0,marker='o')
 
-# ax.plot(0,0,marker='D')
-# ax.plot(0,5,marker='D')
-# ax.plot(-5,5,marker='D')
-# ax.plot(-5,0,marker='D')
+ax.plot(0,0,marker='D')
+ax.plot(0,5,marker='D')
+ax.plot(5,5,marker='D')
+ax.plot(5,0,marker='D')
 # ax.plt.xlim(-1, 1)
 # ax.plt.ylim(-1, 1)
 ax.axis('scaled') #plt.gca().set_aspect('equal', adjustable='box')
 
-plt.ioff()
+
+# plt.ioff()
 plt.show()
